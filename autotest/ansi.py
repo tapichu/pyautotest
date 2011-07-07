e4a5d264396012a2433ad05a40b4ae139927b91b
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-07-07 13:53:40 -0500

    Disable ANSI colors on Windows (at least for now)

--- autotest/ansi.py
+++ autotest/ansi.py
@@ -3,17 +3,22 @@
 Utility functions to wrap strings in ANSI colour codes.
 """
 
+import os
 from sys import stdout
 
 def highlight(string, is_tty=stdout.isatty()):
     """
     Green text.
     """
+    if os.name == 'nt':
+        is_tty = False
     return ('\033[32;1m' + string + '\033[0m') if is_tty else string
 
 def error(string, is_tty=stdout.isatty()):
     """
     Red text.
     """
+    if os.name == 'nt':
+        is_tty = False
     return ('\033[31;1m' + string + '\033[0m') if is_tty else string
 
